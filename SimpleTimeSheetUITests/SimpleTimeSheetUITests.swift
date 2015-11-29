61c59fdbaa1c389eb32e7574e7a5386366f49212
Jonas Sääv <jonas@macbookpro2014.local>
2015-11-29 13:01:22 +0100

    Added rudimentary UITestcase

--- SimpleTimeSheetUITests/SimpleTimeSheetUITests.swift
+++ SimpleTimeSheetUITests/SimpleTimeSheetUITests.swift
@@ -29,6 +29,8 @@ class SimpleTimeSheetUITests: XCTestCase {
     }
     
     func testExample() {
+        XCTAssertTrue(XCUIApplication().staticTexts["Super Simple TimeSheet"].exists)
+        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
